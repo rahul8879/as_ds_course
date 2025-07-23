97b7bc72492ca69f8217a751db17e177c5404d55
rahul120897 <vihaantiwari224@gmail.com>
2025-07-23 20:05:31 +0530

    added the corrected files

---
+++ Functions/6_modules/example.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    """
+    This program adds two numbers and return the result
+    """
+    result = a + b
+    return result
+
+def getName():
+    test ='I am not working'
+    return test
